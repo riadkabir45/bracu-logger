47332a269d9373a0d6798755a19ca6c20a3dec0c
riadkabir45 <riadkabir45@gmail.com>
2023-02-13 15:31:21 +0000

    feat: btnAction

--- app/src/main/java/com/erza/braculogger/MainActivity.java
+++ app/src/main/java/com/erza/braculogger/MainActivity.java
@@ -3,12 +3,29 @@ package com.erza.braculogger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
+
+    class btnActionn implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_LONG).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn_connect);
+
+        btn.setOnClickListener(new btnActionn());
     }
 }
